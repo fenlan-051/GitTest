e9eddc66e6cd3eba0aedcc7757600335e1e4c1d2
situboy <situboy@atguigu.com>
2020-09-14 11:29:32 +0800

    第一次hotfix修改

--- src/com/atguigu/test/GitTest.java
+++ src/com/atguigu/test/GitTest.java
@@ -2,7 +2,7 @@ package com.atguigu.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello");//hotfix第一次修改
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
