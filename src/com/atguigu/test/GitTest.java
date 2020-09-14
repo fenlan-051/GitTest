4e5f635a6542ae24ea399cf6f2ccc494b9adc12c
situboy <situboy@atguigu.com>
2020-09-14 11:30:13 +0800

    第一次master修改

--- src/com/atguigu/test/GitTest.java
+++ src/com/atguigu/test/GitTest.java
@@ -2,7 +2,7 @@ package com.atguigu.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello");//第一次master修改
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
